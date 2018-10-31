a344cff658ffcf0d7ffa75b38067f9982a760edb
xujiajun <me@xujiajun.cn>
2018-10-31 17:20:18 +0800

    add example of extract the URL parameters

--- examples/urlParameters/main.go
+++ examples/urlParameters/main.go
@@ -10,7 +10,12 @@ func main() {
 	mux := gorouter.New()
 	//url parameters match
 	mux.GET("/user/:id", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("match user/:id !"))
+		//get one URL parameter
+		id := gorouter.GetParam(r, "id")
+		//get all URL parameters
+		//id := gorouter.GetAllParams(r)
+		//fmt.Println(id)
+		w.Write([]byte("match user/:id ! get id:" + id))
 	})
 
 	log.Fatal(http.ListenAndServe(":8181", mux))
