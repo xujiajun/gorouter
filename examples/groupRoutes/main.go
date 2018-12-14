e9d092ff347a7231028fda6ecbc64d35ed564355
xujiajun <me@xujiajun.cn>
2018-12-14 16:00:01 +0800

    refactor for example of group routes

--- examples/groupRoutes/main.go
+++ examples/groupRoutes/main.go
@@ -8,13 +8,20 @@ import (
 	"github.com/xujiajun/gorouter"
 )
 
-func usersHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "/api/users")
+func usersEventHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "/users/"+gorouter.GetParam(r, "user")+"/events")
+}
+
+func usersEventPublicHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "/users/"+gorouter.GetParam(r, "user")+"/events/public")
 }
 
 func main() {
 	mux := gorouter.New()
-	mux.Group("/api").GET("/users", usersHandler)
+
+	//users group
+	mux.Group("/users").GET("/:user/events", usersEventHandler)
+	mux.Group("/users").GET("/:user/events/public", usersEventPublicHandler)
 
 	log.Fatal(http.ListenAndServe(":8181", mux))
 }
